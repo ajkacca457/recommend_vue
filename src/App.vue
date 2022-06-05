1efa304bacea65b2549b4896ae11779323f217c2
Avijit Karmaker <ajkacca@gmail.com>
2022-06-06 00:05:43 +0300

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,8 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+font-family: 'Metal';
+color: #2c3e50;
 }
 
 #nav {
@@ -33,10 +30,11 @@ export default {
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: black;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: navy;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,14 @@
 <template>
   
-<div id="nav" class="bg-secondary">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<div id="nav" class="d-flex justify-content-between align-items-center px-2 py-2 px-1">
+  <div class="logo-container">
+    <h4>RECOMMEND.</h4>
+  </div>
+  
+  <div class="list-container">
+     <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+  </div>
   </div>
 
 </template>
@@ -13,6 +19,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+#nav {
+  background-color: #ffe0e8;
+}
 
 </style>
