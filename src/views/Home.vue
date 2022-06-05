6e918225b2a21c6e9facb124cb3a2809844e6cc4
Avijit Karmaker <ajkacca@gmail.com>
2022-06-05 14:43:04 +0300

    update folder structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,25 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="bg-danger text-white">{{message}}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from '@vue/reactivity'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  setup() {
+    let message= ref("Project Under construction");
+    return{
+      message
+    }
   }
 }
 </script>
