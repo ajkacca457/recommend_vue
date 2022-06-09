8d71cf7461b1f13247a64f976ebd7c002d7f58d8
Avijit karmaker <ajkacca@gmail.com>
2022-06-09 10:40:00 +0300

    update create collection form

--- src/views/Createcollection.vue
+++ src/views/Createcollection.vue
@@ -3,29 +3,37 @@
 <form class="w-25 my-5 mx-auto" id="createform">
 
 <div class="logo d-flex align-items-center justify-content-center">
-<font-awesome-icon icon="arrow-right-to-bracket"/>
-<h2 class="text-center" style="margin-left:10px">Login..</h2>
+<font-awesome-icon icon="pen-fancy" />
+<h2 class="text-center" style="margin-left:10px">Create Collection</h2>
 </div>
 
-  <!-- Email input -->
+  <!--input group 1 -->
   <div class="form-outline mb-4">
-    <input type="email" id="form2Example1" class="form-control" />
-    <label class="form-label" for="form2Example1">Email address</label>
+    <label class="form-label" for="coltitle">Name</label>
+    <input type="text" class="form-control" id="coltitle" placeholder="Collection name">
   </div>
 
-  <!-- Password input -->
+  <!--input group 2 -->
   <div class="form-outline mb-4">
-    <input type="password" id="form2Example2" class="form-control" />
-    <label class="form-label" for="form2Example2">Password</label>
+    <label class="form-label" for="coldescription">Description</label>
+    <input type="text" class="form-control" id="coldescription" placeholder="Collection info">
   </div>
+
+<!--input group 3 -->
+      <div class="form-group col-md-4 w-100">
+      <label for="inputcategory" class="mb-2">Category</label>
+      <select id="inputcategory" class="form-control mb-4">
+        <option selected>Movies</option>
+        <option>Books</option>
+        <option>Music</option>
+        <option>Sports Events</option>
+        <option>Tv series</option>
+      </select>
+    </div>
 
   <!-- Submit button -->
-  <button type="button" class="btn btn-primary btn-block mb-4 w-100">Sign in</button>
+  <button type="button" class="btn btn-primary btn-block mb-4 w-100">Create Collection</button>
 
-  <!-- Register buttons -->
-  <div class="text-center">
-    <p>Not a member? <a href="#!">Register</a></p>
-  </div>
 </form>
 
 </template>
