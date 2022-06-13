314cc74483ff2f8bcc723f501efb64b8293658c9
Avijit karmaker <ajkacca@gmail.com>
2022-06-13 11:05:38 +0300

    add current user

--- src/components/Reccollections.vue
+++ src/components/Reccollections.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
+    <div v-if="user"><p>Hello, <strong>{{user.displayName}}</strong></p> </div>
+    <div v-else><p>Hello, <strong>Guest!</strong></p> </div>
+
     <h1>Here are all the recommondation collection. </h1>
 </div>
 
 </template>
 
 <script>
+
+import getCurrentUser from "../composibles/getcurrentUser";
+
 export default {
 
-    name:"Reccollections"
+    name:"Reccollections",
+
+  setup() {
+
+    let {user}= getCurrentUser(); 
+
+    return {
+      user
+    }
+
+  }
 
 
 }
--- src/composibles/getcurrentUser.js
+++ src/composibles/getcurrentUser.js
@@ -1,3 +1,23 @@
 import { ref } from "vue";
+import { app } from "../firebase/config";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-let user= ref();
+let auth= getAuth();
+let user= ref(auth.currentUser);
+
+onAuthStateChanged(auth, (_user)=> {
+    user.value= _user;
+    
+})
+
+
+let getCurrentUser= ()=> {
+
+    return {
+        user
+    }
+
+}
+
+
+export default getCurrentUser;
