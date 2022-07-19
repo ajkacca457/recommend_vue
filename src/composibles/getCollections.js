d5afca48b8a56487c554a92bd9b6ae877efd71c4
Avijit karmaker <ajkacca@gmail.com>
2022-07-19 13:12:15 +0300

    update list of recommendation

--- src/composibles/getCollections.js
+++ src/composibles/getCollections.js
@@ -1,50 +1,38 @@
 import { onSnapshot, orderBy, query } from "firebase/firestore";
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { app, RecCollectionRef } from "../firebase/config";
 
 
 let getCollections=()=> {
 
     let error= ref(null);
-
     let isPending= ref(false);
-
     let documents= ref([]);
 
 
-    let collectDocs= async()=> {
+    let collectDocs=  ()=> {
         error.value= null;
         isPending.value= true;
 
-        try {
-            // let response=await getDocs(RecCollectionRef);    
-
-            // if(!response) {
-            //     throw new Error("Collections are unavailable at the moment.")
-            // }
-            
-            // response.docs.forEach((doc)=> {
-            //     documents.value.push({...doc.data(),id:doc.id});
-            // })
-
             let q= query(RecCollectionRef,orderBy("createdAt"));
-            onSnapshot(q,(snapshot)=> {
+            let unsub=onSnapshot(q,(snapshot)=> {
                 let results=[];
                 snapshot.docs.forEach(doc=> {
                 results.push({...doc.data(), id:doc.id})
                 documents.value=results;
                 error.value=null;
                 })
+            isPending.value=false;
+            error.value= null;            
+            }, (err)=> {
+                documents.value=null;
+                isPending.value=false;
+                error.value=err.message;
             })
-    
-            isPending.value=false;
-            error.value= null;
-
-        } catch (err) {
             
-            error.value= err.message;
-            isPending.value=false;
-        }
+         watchEffect((oninvalidate)=> {
+            oninvalidate(()=>unsub());
+         })   
 
     }
 
