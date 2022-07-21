125bc2fd3826fcbb5813c66b33510cc7b268ea9d
Avijit karmaker <ajkacca@gmail.com>
2022-07-21 20:03:20 +0300

    update form in recommendation detail

--- src/components/AddRecommendation.vue
+++ src/components/AddRecommendation.vue
@@ -1,5 +1,20 @@
 <template>
     <h3>Here will be the Form:</h3>
+    <form>
+<div class="form-group">
+    <label for="Rectitle">Title</label>
+    <input type="text" class="form-control" id="Rectitle" placeholder="add name of recommendation">
+  </div>
+
+  <div class="form-group">
+    <label for="Recinfo">Info</label>
+    <input type="text" class="form-control" id="Recinfo" placeholder="Recommendation info">
+  </div>
+
+    <input type="submit"/> 
+
+    </form>
+
 
 </template>
 
