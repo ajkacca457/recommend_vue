84850dfd40b3b1f450e7b16c58e19c3bac90c580
Avijit karmaker <ajkacca@gmail.com>
2022-07-21 20:11:53 +0300

    update recommendation styling

--- src/components/AddRecommendation.vue
+++ src/components/AddRecommendation.vue
@@ -1,19 +1,21 @@
 <template>
-    <h3>Here will be the Form:</h3>
+<div class="bg-info p-2 rounded">
+    <h3>Add Recommendation:</h3>
     <form>
-<div class="form-group">
+<div class="form-group mt-1">
     <label for="Rectitle">Title</label>
     <input type="text" class="form-control" id="Rectitle" placeholder="add name of recommendation">
   </div>
 
-  <div class="form-group">
+  <div class="form-group mt-1">
     <label for="Recinfo">Info</label>
     <input type="text" class="form-control" id="Recinfo" placeholder="Recommendation info">
   </div>
 
-    <input type="submit"/> 
+    <input type="submit" class="mt-4 bg-success text-white border-0 rounded w-25"/> 
 
     </form>
+</div>
 
 
 </template>
