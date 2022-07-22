e9f6c8b1bdd7af903e0de00113b5c77095777ea3
Avijit Karmaker <ajkacca@gmail.com>
2022-07-23 01:59:12 +0300

    update recommendation list styling

--- src/components/Recommendation.vue
+++ src/components/Recommendation.vue
@@ -2,6 +2,7 @@
     <div class="card p-2">
         <h4>Here is the title</h4>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolore error ex nobis. A quidem accusantium quia atque commodi eos eveniet nesciunt similique error, vero, pariatur aut inventore ducimus cumque!</p>
+        <p class="bg-warning text-center py-2">Rating: Must Try</p>
     </div>
 
 
