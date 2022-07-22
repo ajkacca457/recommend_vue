ae24478301f06ba1c3e1db860889a6643f8c719e
Avijit karmaker <ajkacca@gmail.com>
2022-07-22 14:03:46 +0300

    update add recommendation form

--- src/components/AddRecommendation.vue
+++ src/components/AddRecommendation.vue
@@ -1,15 +1,15 @@
 <template>
 <div class="bg-info p-2 rounded">
     <h3>Add Recommendation:</h3>
-    <form>
+    <form @submit.prevent="handleSubmit">
 <div class="form-group mt-1">
     <label for="Rectitle">Title</label>
-    <input type="text" class="form-control" id="Rectitle" placeholder="add name of recommendation">
+    <input type="text" class="form-control" id="Rectitle" placeholder="add name of recommendation" v-model="title">
   </div>
 
   <div class="form-group mt-1">
     <label for="Recinfo">Info</label>
-    <input type="text" class="form-control" id="Recinfo" placeholder="Recommendation info">
+    <input type="text" class="form-control" id="Recinfo" placeholder="Recommendation info" v-model="info">
   </div>
 
     <input type="submit" class="mt-4 bg-success text-white border-0 rounded w-25"/> 
@@ -21,8 +21,25 @@
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 export default {
-    name:"AddRecommendation"
+    name:"AddRecommendation",
+
+    setup() {
+        let title= ref(null);
+        let info= ref(null);
+
+        let handleSubmit=()=> {
+          console.log(title.value, info.value);
+        }
+
+    return {
+      title, info, handleSubmit
+    }
+
+
+    }
+
 }
 </script>
 
